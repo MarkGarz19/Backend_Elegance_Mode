a2b96dd54d2362a145d053e1c0afe7d820dd038a
Markus <markblake190399@gmail.com>
2024-06-17 10:36:47 +0200

    backend v7.2

--- src/controller/controllerusuario.js
+++ src/controller/controllerusuario.js
@@ -3,9 +3,9 @@ const ModelUsuarios = require('../model/modelusuario');
 
 class UserController {
     static async registerUser(req, res) { // esta funcion asicronica deberia comunicarse con la base de datos para registrar un nuevo usuario
-        const newUser = req.body;
-        const brycpt = require('bcryptjs');
-        const salt = 10;
+        const newUser = req.body; // se declara la variable newUser para el body de la petición
+        const brycpt = require('bcryptjs'); // esta variable es para encriptar la contraseña del usuario
+        const salt = 10; // esta variable es para encriptar la contraseña del usuario en 10 vueltas
 
         const usuarioexistente = await ModelUsuarios.UsarioEmail(newUser.email); // esta variable es para saber si el usuario ya existe
         if (usuarioexistente) {
@@ -13,7 +13,7 @@ class UserController {
         }
 
         const password_hashed = await brycpt.hash(newUser.password, salt); // esta variable es para encriptar la contraseña del usuario
-        const newUser_password = { ...newUser, password: password_hashed };
+        const newUser_password = { ...newUser, password: password_hashed }; // esta variable es para crear nuevos datos del usuario con la contraseña encriptada
         const result = await ModelUsuarios.registerUser(newUser_password); // esta variable es para guardar el nuevo usuario
 
         if (result.error) {
@@ -27,13 +27,13 @@ class UserController {
     static async loginUser(req, res) { // esta funcion asicronica deberia comunicarse con la base de datos para iniciar sesion
         try {
             const { email, password } = req.body; // se declara la variable email y password para el body de la petición
-            const result = await ModelUsuarios.loginUser({ email, password });
+            const result = await ModelUsuarios.loginUser({ email, password }); // esta variable es para iniciar la sesion con el email y la contraseña
+            const confirmPassword = await brycpt.compare(password, result.data.password);
 
-            if (result.error) {
+            if (result.error && confirmPassword.error) {
                 return res.status(401).json({ message: result.message });
             }
-
-            return res.status(200).json({ message: 'Inicio de sesión exitoso', id: result.data._id });
+                return res.status(200).json({ message: 'Inicio de sesión exitoso', id: result.data._id });
         } catch (error) {
             res.status(500).json({ message: 'Error del servidor', error: error.message });
         }
