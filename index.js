29ce92d15685f5b05fddb662b542ada8826bb26f
Markus <markblake190399@gmail.com>
2024-06-17 17:25:33 +0200

    backend v17

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const server = express();
 const dotenv = require('dotenv');
 const rouuter = require('./src/router/router');
 const PAYPAL_API = 'https://api-m.sandbox.paypal.com'
+const cors = require('cors');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-server.use((req, res, next) => { // esto es para que se puedan hacer peticiones sin tener problemas con las configuraciones de cors
-    res.setHeader('Access-Control-Allow-Origin', '*'); // para que se puedan hacer peticiones desde cualquier lugar
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,  Authorization');
-    next()
-})
+const corsOptions = { origin: ['*'], method: ['GET', 'POST'], allowHeaders: ['Content-Type', 'Authorization'] }
+
+server.use(cors(corsOptions));
 
 server.use(express.json()); // para poder leer el body de las peticiones
 
