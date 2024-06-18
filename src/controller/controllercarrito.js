748ee522e874fcbc2834595965032acffe008bf6
Markus <markblake190399@gmail.com>
2024-06-18 09:41:39 +0200

    backend v27

--- src/controller/controllercarrito.js
+++ src/controller/controllercarrito.js
@@ -7,15 +7,15 @@ const client = new paypal.core.PayPalHttpClient(new Environment(process.env.PAYP
 
 class ControllerCarrito { // la clase controladora de carrito
     static async compraProductoCarrito(req, res) {
-        const { items, total, metodoDePago } = req.body; // Obtener los items, total y metodo de pago del cuerpo de la petición
+        const { items, total, metodoDePago, user_id } = req.body; // Obtener los items, total y metodo de pago del cuerpo de la petición
 
         const fecha_Pedido = new Date().toLocaleDateString('es-ES', { timeZone: 'UTC', }); // Obtener la fecha actual en formato de cadena
         const time = new Date().toLocaleTimeString('es-ES', { timeZone: 'UTC', hour: '2-digit', minute: '2-digit' }); // Obtener la hora en formato de cadena
         try {
             for (const item of items) {
                 const { title, price, quantity } = item; // Extraer el título, precio y cantidad de cada artículo
+                const carritonew = { Producto: title, Precio: price, Cantidad: quantity, Total: total, Fecha: fecha_Pedido, Hora: time, metodoDePago: metodoDePago, user_id: user_id, created: new Date(), update: new Date() };
                 const resultado = await Modelcarrito.newCarrito(carritonew);
-
                 if (!resultado) {
                     return res.status(400).json({ message: 'No se ha podido realizar la compra' });
                 }
@@ -56,10 +56,10 @@ class ControllerCarrito { // la clase controladora de carrito
 
                     const order = await client.execute(request);
                     const redirectUrl = order.result.links.find(link => link.rel === 'approve').href; // esta variable indica si la orden de compra fue existosa o no se readireccionara a las urls indicadas
-                    const Order_PayPal = order.result.id;
-                    await carritonew.create({
-                        Producto: title, Precio: price, Cantidad: quantity, total: total + ' USD', metododepago: metodoDePago, fecha_Pedido: fecha_Pedido + ' ' + time, created_at: new Date(), updated_at: new Date()
-                    })
+                    const OrderPaypal = order.result.id;
+                    resultado.paypal_order_id = OrderPaypal;
+
+                    await resultado.save();
                     res.status(200).json({ redirectUrl });
                 }
             }
